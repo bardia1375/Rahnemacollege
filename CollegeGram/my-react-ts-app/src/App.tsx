ff0190666f50631da9d8d760c865e5bcd87e4a41
Reza Elahi <68402981+rezelahi@users.noreply.github.com>
2024-08-05 18:07:34 +0330

    initial reza's branch

--- CollegeGram/my-react-ts-app/src/App.tsx
+++ CollegeGram/my-react-ts-app/src/App.tsx
@@ -7,18 +7,16 @@ import Register from "./Pages/Register";
 import ForgetPassword from "./Pages/ForgetPassword";
 import SetNewPassword from "./Pages/SetNewPassword";
 import "./index.css"; // Ensure this is imported
+import Dashboard from "./Pages/Dashboard";
 
 const App: React.FC = () => {
   return (
     <AppProvider>
       <Router>
-        <div
-          className="bg-login-background bg-cover bg-center h-screen flex items-center justify-center"
-         
-        >
+        <div className="bg-login-background bg-cover bg-center h-screen flex items-center justify-center">
           <Routes>
-            <Route path="/" element={<Home />} />
             <Route path="/about" element={<Login />} />
+            <Route path="/" element={<Dashboard />} />
           </Routes>
         </div>
       </Router>
---
+++ CollegeGram/my-react-ts-app/src/Pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <div className="size-20 bg-red-500">Dashboard</div>;
+}
+
+export default Dashboard;
